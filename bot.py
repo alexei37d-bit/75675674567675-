97f5c9f6d1b0663da8b94a5d617bc0e626e821aa
alexei37d-bit <alexei37d@gmail.com>
2026-07-27 11:51:43 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-# Замените 'ВАШ_ТОКЕН_БОТА' на токен, полученный от @BotFather
+# Тестовый токен
 TOKEN = "8740242990:AAF2I7c7x_SD6-Dww3WQJKQYbk3WsXYP6BI"
 
 dp = Dispatcher()
@@ -11,13 +11,13 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    # Получаем имя пользователя (или 'Игрок', если имя не указано)
+    # Получаем имя пользователя
     user_name = message.from_user.first_name if message.from_user else "Игрок"
 
-    # Экранируем имя пользователя для безопасности и делаем текст жирным
-    text = f"<b><tg-emoji emoji-id=\"5472419592217332357\">🔥</tg-emoji> Добро пожаловать, {html.quote(user_name)}!</b>"
+    # Экранируем имя и формируем жирный текст с эмодзи
+    text = f'<b><tg-emoji emoji-id="5472419592217332357">🔥</tg-emoji> Добро пожаловать, {html.quote(user_name)}!</b>'
 
-    # Отправляем сообщение с поддержкой HTML-разметки
+    # Отправляем сообщение
     await message.answer(text, parse_mode="HTML")
 
 
