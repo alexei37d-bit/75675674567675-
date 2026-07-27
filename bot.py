156fc66f0f03a416c1439a3ebd735248004fabec
alexei37d-bit <alexei37d@gmail.com>
2026-07-27 13:15:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,30 @@
 import asyncio
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, F, html
 from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, Message
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+)
 
 TOKEN = "8740242990:AAF2I7c7x_SD6-Dww3WQJKQYbk3WsXYP5BI"
 
 dp = Dispatcher()
 
+
 main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(
-                text="Кошелек",
-                icon_custom_emoji_id="5197686464325915345"
+                text="Кошелек", icon_custom_emoji_id="5197686464325915345"
             ),
             KeyboardButton(
-                text="Играть",
-                icon_custom_emoji_id="5471895876790161593"
+                text="Играть", icon_custom_emoji_id="5471895876790161593"
             ),
             KeyboardButton(
-                text="Меню",
-                icon_custom_emoji_id="5469969339144773395"
+                text="Меню", icon_custom_emoji_id="5469969339144773395"
             ),
         ]
     ],
@@ -28,13 +32,44 @@ main_keyboard = ReplyKeyboardMarkup(
 )
 
 
+wallet_inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="Пополнить",
+                icon_custom_emoji_id="5255805270285653933",
+                callback_data="deposit",
+            ),
+            InlineKeyboardButton(
+                text="Вывести",
+                icon_custom_emoji_id="5255868234506213301",
+                callback_data="withdraw",
+            ),
+        ]
+    ]
+)
+
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     user_name = message.from_user.first_name if message.from_user else "Игрок"
-
     text = f'<b><tg-emoji emoji-id="5472419592217332357">🔥</tg-emoji> Добро пожаловать, {html.quote(user_name)}!</b>'
 
     await message.answer(text, parse_mode="HTML", reply_markup=main_keyboard)
+
+
+@dp.message(F.text.in_(["Кошелек", "Баланс", "/wallet", "/balance"]))
+async def wallet_handler(message: Message) -> None:
+    text = (
+        '<tg-emoji emoji-id="5470019396988606408">💵</tg-emoji> '
+        "Баланс: <code>0.00</code>$"
+    )
+
+    await message.answer(
+        text=text,
+        parse_mode="HTML",
+        reply_markup=wallet_inline_keyboard,
+    )
 
 
 async def main() -> None:
